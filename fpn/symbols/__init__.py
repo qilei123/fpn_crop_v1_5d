350823a4c46026dbbcc9c5546a2ade4b86cb74d3
qilei123 <qileimail123@gmail.com>
2019-03-02 16:27:52 -0500

    fdafd

--- fpn/symbols/__init__.py
+++ fpn/symbols/__init__.py
@@ -1,16 +1,1 @@
-import resnet_v1_101_fpn_rcnn
-import resnet_v1_101_fpn_dcn_rcnn
-import resnet_v1_101_fpn_rcnn_l1
-import resnet_v1_101_fpn_rcnn_l0
-import resnet_v1_101_fpn_dcn_rcnn_l0
-import resnet_v1_101_fpn_dcn_rcnn_l1
-import resnet_unet_rcnn_l0
-import resnet_v1_50_fpn_rcnn_l0
-import vgg_16_fpn_rcnn_l0
 import resnet_v1_101_fpn_rcnn_l2_focal
-import resnet_v1_101_fpn_rcnn_l0_focal
-import resnet_v1_101_fpn_dcn_rcnn_l0_focal
-import resnet_v1_50_fpn_rcnn_l0_focal
-import resnet_v1_101_fpn_dcn_rcnn_l2_focal
-import vgg_16_fpn_rcnn_l0_focal
-import vgg_16_fpn_rcnn_l1_focal
