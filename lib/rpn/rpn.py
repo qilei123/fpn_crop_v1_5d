24878c4514cf56f6f5df8b2d49549faf22f5ffe3
qilei123 <qileimail123@gmail.com>
2019-03-06 23:57:43 -0500

    fda

--- lib/rpn/rpn.py
+++ lib/rpn/rpn.py
@@ -404,7 +404,7 @@ def assign_pyramid_anchor(feat_shapes, gt_boxes, im_info, cfg, feat_strides=(4, 
             for j in range(crop_nums):
                 temp_fpn_anchors = fpn_anchors[j*total_anchors/crop_nums:(j+1)*total_anchors/crop_nums,:]
                 temp_gt_boxes = np.zeros(gt_boxes.shape)
-                temp_gt_boxes[np.where(gt_boxes[5]==j),:]=gt_boxes[np.where(gt_boxes[5]==j),:]
+                temp_gt_boxes[np.where(gt_boxes[:,5]==j),:]=gt_boxes[np.where(gt_boxes[:,5]==j),:]
                 temp_argmax_overlaps = argmax_overlaps[j*total_anchors/crop_nums:(j+1)*total_anchors/crop_nums]
                 temp_fpn_labels = fpn_labels[j*total_anchors/crop_nums:(j+1)*total_anchors/crop_nums]
                 temp_fpn_bbox_targets = np.zeros((total_anchors/crop_nums, 4), dtype=np.float32)
