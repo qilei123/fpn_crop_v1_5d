40336f5b26fc34260d672a71f87051d0757ec82a
qilei123 <qileimail123@gmail.com>
2019-03-06 23:55:54 -0500

    fda

--- lib/rpn/rpn.py
+++ lib/rpn/rpn.py
@@ -346,8 +346,6 @@ def assign_pyramid_anchor(feat_shapes, gt_boxes, im_info, cfg, feat_strides=(4, 
             feat_height, feat_width, A, total_anchors = fpn_args[i]
             for j in range(crop_nums):
                 temp_gt_boxes = np.zeros(gt_boxes.shape)
-                print "gt_boxes"
-                print gt_boxes
                 temp_gt_boxes[np.where(gt_boxes[:,5]==j),:]=gt_boxes[np.where(gt_boxes[:,5]==j),:]
                 overlaps = np.vstack((overlaps,
                         bbox_overlaps(fpn_anchors[j*total_anchors/crop_nums:(j+1)*total_anchors/crop_nums,:].astype(np.float),
