4398b3b916d47c392f3b01898b3a5e7cf2e4f973
qilei123 <qileimail123@gmail.com>
2019-03-07 15:55:39 -0500

    fda

--- fpn/operator_py/pyramid_proposal.py
+++ fpn/operator_py/pyramid_proposal.py
@@ -236,13 +236,12 @@ class PyramidProposalOperator(mx.operator.CustomOp):
         keeps = np.zeros(0)
         for i in range(crop_nums):
             channel_index = np.where(channel_records==i)
-            temp_proposals = proposals[channel_index,:]
+            temp_ch_proposals = proposals[channel_index,:]
             print proposals.shape
-            print temp_proposals.shape
-            print temp_proposals.shape
+            print temp_ch_proposals.shape
             temp_scores = scores[channel_index]
             print temp_scores.shape
-            det = np.hstack((temp_proposals, temp_scores)).astype(np.float32)
+            det = np.hstack((temp_ch_proposals, temp_scores)).astype(np.float32)
             keep = nms(det)
             if post_nms_topN > 0:
                 keep = keep[:post_nms_topN]
